848d9ec929614867281c4878c0661d60931c4670
Pablo Hidalgo Martínez <21647189@live.uem.es>
2017-01-30 17:13:25 +0100

    Tests v2

--- src/enviroments_github_tests/Enviroments_github_tests.java
+++ src/enviroments_github_tests/Enviroments_github_tests.java
@@ -4,14 +4,14 @@ public class Enviroments_github_tests {
 	public static void main(String[] args) {
 		int n1 = 0;
 		int n2 = 0;
-		char ast = '*';
+		char ast = '1';
 		while (n1 < 5) {
 			n1++;
 			while (n2 < 4) {
 				n2++;
-				System.out.print(ast + " ");
+				System.out.print(ast++ + " ");
 			}
-			System.out.println(ast);
+			System.out.println(ast++);
 			n2 = 0;
 		}
 	}
